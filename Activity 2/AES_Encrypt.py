63011c89a952497e2661a7ebd109df07e6277965
Connor <50525187+WobblyRonnoc@users.noreply.github.com>
2023-07-27 09:55:46 -0400

    Polished output. Ready to demo!

--- Activity 2/AES_Encrypt.py
+++ Activity 2/AES_Encrypt.py
@@ -12,38 +12,39 @@ print("Enter your username and password to encrypt")
 username = input("Username: ")
 password = input("Password: ")
 
-print("\n\n")
-print("\t* encrypting the data".center(50, "="))
+print("\n")
+print("Encrypting the data".center(50, "="))
 
 # encode the data to bytes
-print("\t* encoding username and password to bytes...")
+print("* encoding username and password to bytes...")
 username = username.encode()
 password = password.encode()
 
 
 # pad the user and password so that it is a multiple of 16 bytes
-print("\t* padding username and password...")
+print("* padding username and password...")
 username = pad(username, AES.block_size)
 password = pad(password, AES.block_size)
 
 
 # encrypt the data
-print("\t* encrypting username and password...")
+print("* encrypting username and password...")
 encrypted_username = cipher.encrypt(username)
 encrypted_password = cipher.encrypt(password)
 
 # encode the encrypted data to base64 before decoding to utf-8 to
+print("* encoding the encrypted data to base64 before decoding to utf-8...")
 ct_username = b64encode(encrypted_username).decode('utf-8')
 ct_password = b64encode(encrypted_password).decode('utf-8')
 
 # get the initialization vector
+print("* getting the initialization vector...")
 iv = b64encode(cipher.iv).decode('utf-8')
 
 # print the encrypted data
 print('=' * 50)
-print(f"Encrypted Username and Password")
-print(f"Username: {ct_username}")
-print(f"Password: {ct_password}")
+print(f"Encrypted Username: {ct_username}")
+print(f"Encrypted Password: {ct_password}")
 
 # decrypt the data
 
@@ -52,30 +53,32 @@ print("Decrypting the data".center(50, "="))
 
 
 # decode the base64 encoded initialization vector
-print("decoding the base64 encoded initialization vector...")
+print("* decoding the base64 encoded initialization vector...")
 iv = b64decode(iv)
 
 # decode the base64 encoded encrypted ciphertexts
-print("decoding the base64 encoded encrypted ciphertexts...")
+print("* decoding the base64 encoded ciphertexts...")
 ct_username = b64decode(ct_username)
 ct_password = b64decode(ct_password)
 
 # using the initialization vector, create a new cipher object
-print("using the initialization vector to create a new cipher object...")
+print("* using the initialization vector to create a new cipher object...")
 cipher = AES.new(key, AES.MODE_CBC, iv)
 
 # unpad and decrypt the ciphertexts to get the plaintext username and password
-print("unpadding and decrypting the ciphertexts...")
+print("* unpadding and decrypting the ciphertexts...")
 pt_username = unpad(cipher.decrypt(ct_username), AES.block_size)
 pt_password = unpad(cipher.decrypt(ct_password), AES.block_size)
 
 # decode the bytes back into strings
-print("decoding the bytes back into strings...")
+print("* decoding the bytes back into strings...")
 pt_username = pt_username.decode()
 pt_password = pt_password.decode()
 
 # print the plaintext username and password
 print('=' * 50)
-print(f"Decrypted Username and Password")
-print(f"Username: {pt_username}")
-print(f"Password: {pt_password}")
+print(f"Decrypted Username: {pt_username}")
+print(f"Decrypted Password: {pt_password}")
+
+print("\n\nExiting...")
+
